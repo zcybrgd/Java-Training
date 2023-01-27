5ac6c6ed009cbb0bd4a2da08ddb0c6ba48a5e168
fntysm <zainebb7k9h1@gmail.com>
2023-01-27 11:01:29 +0100

    OOP1

--- src/App.java
+++ src/App.java
@@ -2,9 +2,10 @@ import java.util.Scanner;
 public class App{
     public static int factorial(int n){
         if(n>1){
-            n=n*factorial(n-1);
+            return n*factorial(n-1);
+        }else{
+            return 1;
         }
-        return n;
     }
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
