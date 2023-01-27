76e24cb906de104feaceaad01ab85b5150636b1d
fntysm <zainebb7k9h1@gmail.com>
2023-01-27 10:53:49 +0100

    --

--- src/App.java
+++ src/App.java
@@ -1,5 +1,17 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import java.util.Scanner;
+public class App{
+    public static int factorial(int n){
+        if(n>1){
+            n=n*factorial(n-1);
+        }
+        return n;
+    }
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int n = input.nextInt();
+        int result = factorial(n);
+        System.out.println("the factorial of : " + n + " is : " +result);
+        input.close();
     }
 }
