d10db01b5cee8735a9773612ecca9f35dc546751
fntysm <zainebb7k9h1@gmail.com>
2023-02-25 14:20:50 +0100

    --partie 2

--- src/td2exo6/Etudiant.java
+++ src/td2exo6/Etudiant.java
@@ -3,7 +3,7 @@ package td2exo6;
 public class Etudiant extends Personne {
     private int matricule;
     private double[] notes;
-    private static int nbrModules=3;
+    private static int nbrModules;
     public Etudiant(String n, String p, short a, int mat, double[] notes){
         super(n, p, a);
         this.matricule = mat;
@@ -11,10 +11,13 @@ public class Etudiant extends Personne {
     }
     public double calculMoyenne(){
         double moy=0;
+        nbrModules = notes.length;
         for(double note: notes){
-            moy=+note;
+            moy+=note;
+            System.out.println("+ note " + moy);
         }
         moy/=nbrModules;
+        System.out.println("+ finale " + moy);
         return moy;
     }
     public void afficher(){
--- src/td2exo6/MainApp.java
+++ src/td2exo6/MainApp.java
@@ -2,15 +2,37 @@ package td2exo6;
 import java.util.Scanner;
 public class MainApp {
     public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
-        System.out.println("Veuillez entrer le nom de la personne: ");
-        String nom = input.nextLine();
-        System.out.println("Veuillez entrer le prénom de la personne: ");
-        String prenom = input.nextLine();
-        System.out.println("Veuillez entrer l'age de la personne: ");
-        short age = input.nextShort();
-        input.close();
-        Personne somebody = new Personne(nom, prenom, age);
-        somebody.afficher();
+        int question = 2;
+        switch(question){
+            case 1:{
+                Scanner input = new Scanner(System.in);
+                System.out.println("Veuillez entrer le nom de la personne: ");
+                String nom = input.nextLine();
+                System.out.println("Veuillez entrer le prénom de la personne: ");
+                String prenom = input.nextLine();
+                System.out.println("Veuillez entrer l'age de la personne: ");
+                short age = input.nextShort();
+                input.close();
+                Personne somebody = new Personne(nom, prenom, age);
+                somebody.afficher();
+            }break;
+            case 2:{
+                String[] mod1 = {"ANA3","ANA4"};
+                String[] mod2 = {"ARCHI1","POO"};
+                double[] notes1 = {17, 11, 15.5 , 18.25};
+                double[] notes2 = {14, 13, 15.75 , 16};
+                double[] notes3 = {17, 10.25, 19, 13.5};
+                Enseignant Ens1 = new Enseignant("Lounis", "Farhat", (short) 37, 1472575848L, mod1);
+                Enseignant Ens2 = new Enseignant("Bousbia", "Nabila", (short) 42, 2832758548L, mod2);
+                Etudiant Stu1 = new Etudiant("Boukhetala", "Zaineb", (short) 18, 210052,notes1);
+                Etudiant Stu2 = new Etudiant("Mallek", "Dina", (short) 20, 200108,notes2);
+                Etudiant Stu3 = new Etudiant("Guittone", "Djihene", (short) 19, 210057,notes3);
+                Personne[] tableauPersonne = {Ens1, Ens2, Stu1, Stu2, Stu3};
+                for(Personne eachPerso : tableauPersonne){
+                    System.out.println("------------------------------");
+                    eachPerso.afficher();
+                }
+            }
+        }
     }
 }
