6aa66950f6be7f0b33eebbb26a024abb9040054d
fntysm <zainebb7k9h1@gmail.com>
2023-03-05 21:49:50 +0100

    --sunday

---
+++ src/td2exo7/Album.java
@@ -0,0 +1,9 @@
+package td2exo7;
+public class Album {
+    private String title;
+    private String Author;
+    private String prodMusic;
+    private int anneeEdition;
+    private String[] titres;
+    private float prix;
+}
