2562e20d08d6ec413139b76a2f95066dabfcb862
fntysm <zainebb7k9h1@gmail.com>
2023-04-22 22:18:47 +0100

    --

---
+++ src/td3exo4/ExpressionParenthesee.java
@@ -0,0 +1,11 @@
+package td3exo4;
+
+public class ExpressionParenthesee {
+    private String expression;
+    public ExpressionParenthesee(String s){
+        this.expression = s;
+    }
+    public boolean isCorrect(){
+        return true;
+    }
+}
---
+++ src/td3exo4/Pile.java
@@ -0,0 +1,21 @@
+package td3exo4;
+
+import java.util.*;
+public class Pile {
+private Deque<Character>pile=new ArrayDeque<Character>(); //ligne 5
+// empile x au sommet de la pile
+public void empiler(char x){ 
+  pile.addFirst(x);
+}
+/*retourne l’élément qui est au sommet de la pile en le supprimant de la pile. Elle 
+lance une exception si la pile est vide*/
+public int depiler() throws PileVideException{ //ligne 13
+if (this.estVide()) throw new PileVideException(); //ligne 14
+return pile.removeFirst(); //ligne 15
+}
+//retourne true si la pile est vide et false dans le cas contraire
+public boolean estVide() {
+return pile.isEmpty(); //ligne 19
+} 
+}
+class PileVideException extends Exception{}
